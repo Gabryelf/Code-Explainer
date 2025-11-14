4cb13633939c57c8073a3b37542c0747894d965a
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2025-11-14 22:09:33 +0300

    v1_fix_root

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from app.ai_hf_service import CodeExplainerAI
+from ai_hf_service import CodeExplainerAI
 
 
 app = FastAPI(title="Code Explainer")
